b1a85338a9fea730b0c48c93620c39a4a5e4dd1d
Elkin77 <epradag00@gmail.com>
2017-10-13 12:36:29 -0500

    Tercer Commit, Elkin Prada. Se solucionan los errores de migración

--- GOIAP_v_1.0/GOIAP/apps/Contador/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContadorConfig(AppConfig):
-    name = 'Contador'
--- GOIAP_v_1.0/GOIAP/apps/Inversionista/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InversionistaConfig(AppConfig):
-    name = 'Inversionista'
