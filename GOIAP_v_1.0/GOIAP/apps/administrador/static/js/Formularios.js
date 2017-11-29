58262f502531ba801e2d1e7a420dac1008cee4bf
Elkin77 <epradag00@gmail.com>
2017-11-29 15:49:58 -0500

    Otras validaciones en formularios

--- GOIAP_v_1.0/GOIAP/apps/administrador/static/js/Formularios.js
+++ GOIAP_v_1.0/GOIAP/apps/administrador/static/js/Formularios.js
@@ -19,7 +19,7 @@ function validar(){
 		alert("Usuario, Nombre o Apellido demasiado largo!!");
 		return false;
 	}
-	else if (password.length > 100 || email.length > 150 ){
+	else if (password.length > 128 || email.length > 150 ){
 		alert("Nombre o Apellido demasiado largo!!");
 		return false;
 	}
@@ -39,4 +39,50 @@ function validar(){
 		alert("No es un Usuario valido!!");
 		return false;
 	}
+}
+
+function validarTwo(){
+	var descripcion;
+	descripcion = document.getElementById("descripcion").value;
+
+	if(descripcion === ""){
+		alert("Faltan descripción por Completar!!");
+		return false;
+	}
+	else if (descripcion.length > 300){
+		alert("Descripción demasiado larga!!");
+		return false;
+	}
+
+}
+
+function validarThree(){
+	var nombre, tipoDoc, archivo, nroPag, descripcion;
+	nombre  = document.getElementById("nombre").value;
+	tipoDoc = document.getElementById("tipoDoc").value;
+	archivo = document.getElementById("archivo").value;
+	nroPag = document.getElementById("nroPag").value;
+	descripcion = document.getElementById("descripcion").value;
+
+	expresionCorreo = /\w+@\w+\.+[a-z]/;
+	expresionNombre = /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/;
+	expresionUsuario = /^[a-z\d_]{4,15}$/i;
+
+	if(nombre === "" || tipoDoc === "" || archivo === "" || nroPag === "" || descripcion === "" ){
+		alert("Faltan Campos por Completar!!");
+		return false;
+	}
+	else if (nombre.length > 50){
+		alert("Nombre muy largo!!");
+		return false;
+	}
+	else if (descripcion.length > 300){
+		alert("Descripción demasiado larga!!");
+		return false;
+	}
+	else if (isNaN(nroPag)){
+		alert("Numero de Páginas debe ser un numero!!");
+		return false;
+	}
+
 }
